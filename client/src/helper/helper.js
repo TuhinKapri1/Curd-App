608c4e739c94e37df0462ee7449d7ea4741a542c
Tuhin Kapri <tuhin.kapri@hih7.com>
2024-08-20 10:25:20 +0530

    url change

--- client/src/helper/helper.js
+++ client/src/helper/helper.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3000/api/v1";
+const baseUrl = "https://curd-app-18dm.onrender.com/api/v1";
 export const axiosInstance = axios.create({
   baseURL: baseUrl,
 });
